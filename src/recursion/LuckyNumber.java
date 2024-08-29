a43d45566980ea87a1db896cb01711f4e85362f1
vijay9897 <vijay.kumar@droom.in>
2024-08-29 23:51:12 +0530

    lucky number problem solved

--- src/recursion/LuckyNumber.java
+++ src/recursion/LuckyNumber.java
@@ -2,11 +2,22 @@ package recursion;
 
 public class LuckyNumber {
 
-    public static void main(String[] args) {
+    public static int counter = 2;
 
+    public static void main(String[] args) {
+        System.out.println(isLucky(74));
     }
 
     public static boolean isLucky(int n) {
+        if (counter > n) {
+            return true;
+        }
 
+        if (n % counter == 0) {
+            return false;
+        }
+        int next = n - (n / counter);
+        counter++;
+        return isLucky(next);
     }
 }
